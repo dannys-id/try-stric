8af777ed81df35d51bcb300d86c70bac2159ea6b
dannys <danizwijaya2000@gmail.com>
2022-04-14 04:56:43 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green[500],
+            title: Center(child: Text("STRIC")),
+            flexibleSpace: Image(
+              image: AssetImage('assets/images/logo.png'),
+            ),
+          ),
+          body: Container(
+            color: Colors.green[500],
+          )),
+    );
+  }
+}
